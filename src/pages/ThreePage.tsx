a370a1f718a32a2cda89c4e560a1d47cba78ded2
JungJihun1012 <jihunjeong965@gmail.com>
2024-07-18 02:15:50 +0900

    :sparkles: ThreePage MenuBar 추가

--- src/pages/ThreePage.tsx
+++ src/pages/ThreePage.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 import React from "react";
 import BoxScene from "../components/ThreeScene/BoxScene";
+import { MenuBar } from "../components/MenuBar/MenuBar";
 
 
 const ThreePage: React.FC= () => {
     return(
         <Container>
+            <MenuBar />
             <BoxScene />
         </Container>
     )
